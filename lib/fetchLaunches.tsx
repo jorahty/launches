64a04a1b37c4f8b42a2396a15b9a46db7c32704d
James Tennant <jorahty@gmail.com>
2024-02-22 10:36:17 -0800

    revalidate data

--- lib/fetchLaunches.tsx
+++ lib/fetchLaunches.tsx
@@ -1,5 +1,7 @@
 export async function fetchLaunches() {
-  const response = await fetch('https://fdo.rocketlaunch.live/json/launches/next/5');
+  const response = await fetch('https://fdo.rocketlaunch.live/json/launches/next/5', {
+    cache: 'no-store',
+  });
   const data = await response.json();
   const launches = data.result;
   return launches;
