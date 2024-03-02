82d4a609b7e17050314481b5eca46a0052fc6d63
James Tennant <jorahty@gmail.com>
2024-03-02 14:38:24 -0800

    use shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,68 @@
-import { fetchLaunches } from '@/lib/fetchLaunches';
-import { LaunchList } from '@/components/LaunchList';
+import { Clock, MapPin, Rocket } from "lucide-react";
+import { fetchLaunches } from "@/lib/fetchLaunches";
+import { Card } from "@/components/ui/card";
 
 export default async function Home() {
   const launches = await fetchLaunches();
   return (
-    <main>
-      <div>
-        <h2>Upcoming Launches</h2>
-        <LaunchList launches={launches} />
+    <main className="mx-auto flex h-screen max-w-xl flex-col gap-4 p-8">
+      <h1 className="text-2xl font-bold">Upcoming Launches</h1>
+      <div className="flex flex-col gap-4">
+        {launches.map((launch: any) => {
+          const place = launch.pad.location.statename ?? launch.pad.location.country;
+          const location = `${place} • ${launch.pad.location.name}`;
+          let time = `${launch.date_str}`;
+          if (launch.t0) {
+            const date = new Date(launch.t0);
+            time += ` • ${date.toLocaleTimeString([], { timeStyle: "short" })}`;
+          }
+          return (
+            <LaunchCard
+              key={launch.id}
+              provider={launch.provider.name}
+              vehicle={launch.vehicle.name}
+              title={launch.name}
+              location={location}
+              time={time}
+            />
+          );
+        })}
       </div>
+      <div className="grow" />
       <p>
-        Data by <a href="https://www.rocketlaunch.live/api">RocketLaunch.Live</a>.
+        Data by{" "}
+        <a
+          className="font-medium text-blue-600 dark:text-blue-400"
+          href="https://www.rocketlaunch.live/api"
+        >
+          RocketLaunch.Live
+        </a>
       </p>
     </main>
+  );
+}
+
+function LaunchCard({ provider, vehicle, title, location, time }: any) {
+  return (
+    <div className="flex gap-3">
+      <Card className="flex w-40 flex-col items-center justify-center bg-secondary p-2">
+        <p className="font-bold">{provider}</p>
+        <div className="flex items-center gap-1">
+          <Rocket size={20} />
+          <p>{vehicle}</p>
+        </div>
+      </Card>
+      <div className="w-full">
+        <p className="font-bold">{title}</p>
+        <div className="flex items-center gap-1">
+          <MapPin size={20} />
+          <p className="w-full">{location}</p>
+        </div>
+        <div className="flex items-center gap-1">
+          <Clock size={20} />
+          <p className="w-full">{time}</p>
+        </div>
+      </div>
+    </div>
   );
 }
--- lib/fetchLaunches.ts
+++ lib/fetchLaunches.ts
@@ -1,6 +1,6 @@
 export async function fetchLaunches() {
-  const response = await fetch('https://fdo.rocketlaunch.live/json/launches/next/5', {
-    cache: 'no-store',
+  const response = await fetch("https://fdo.rocketlaunch.live/json/launches/next/5", {
+    cache: "no-store",
   });
   const data = await response.json();
   const launches = data.result;
